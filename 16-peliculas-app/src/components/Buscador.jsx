9735ac93f27aa2070aa983abf4ebd3dd81325106
Fernando Cardona <fernando.cardona@the-lion.com>
2022-12-23 14:51:16 +0100

    bug busqueda en el componente buscador

--- 16-peliculas-app/src/components/Buscador.jsx
+++ 16-peliculas-app/src/components/Buscador.jsx
@@ -1,4 +1,5 @@
 //IMPORTACIONES DE REACT:
+import { useState } from "react";
 
 
 //IMPORTACIONES DE TERCEROS:
@@ -11,17 +12,35 @@
 export const Buscador = () => {
 
 
+    const handleInputChangeBusqueda = (e) => {
+        const [ busqueda, setBusqueda ] = useState('');
 
+        //Crear estado y actualizarlo
+        setBusqueda(e.target.value);
+        //Obtener listado completo de peliculas
+
+        //Filtrar para buscar coincidencias
+
+        //comprobar si tengo algun resultado
+
+        //Dar valor al localSstorage
+
+        /// Actializarel estado del listado principal con el filtrado 
+
+
+    }
 
     return (
         <div className="search">
-            <h3 className="title">Buscador</h3>
+            <h3 className="title">Buscador: <strong>{ state.busqueda }</strong></h3>
             <form>
                 <input 
                     type="text" 
                     id="search_field" 
                     name="busqueda"
                     autoComplete="off"
+                    value={ state.busqueda }
+                    onChange={ handleInputChangeBusqueda }
                 />
                 <button id="search">Buscar</button>
             </form>
